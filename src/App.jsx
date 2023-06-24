fff758b88c9c1cf1f9a5e01035a24fb6fa0cccb5
TheNinjan <theninjan.com@gmail.com>
2023-06-24 12:29:25 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import "./styles/app.scss";
+import Home from "./components/Home";
+import Header from "./components/Header";
+import Cart from "./components/Cart";
+function App() {
+  return (
+    <>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+        <Toaster />
+      </Router>
+    </>
+  );
+}
+
+export default App;
